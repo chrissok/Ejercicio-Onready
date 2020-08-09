4924806a32f61081e732ce4ada11a3ef8fbb2b89
cri cri <depa@live.com.ar>
2020-08-09 12:17:05 -0300

    .

--- index.js
+++ index.js
@@ -4,11 +4,10 @@ const ListaDeVehiculos = require("./clases/ListaDeVehiculos");
 
 let listaDeVehiculos = new ListaDeVehiculos();
 
-const auto1 = new Auto("Peugeot","206",4,200000.00);
-const moto1 = new Moto("Honda","Titan","125c",60000.00);
-const auto2 = new Auto("Peugeot","208",5,250000.00);
-const moto2 = new Moto("Yamaha","YBR","160c",80500.50);
-
+const auto1 = new Auto("Peugeot", "206", 4, 200000.0);
+const moto1 = new Moto("Honda", "Titan", "125c", 60000.0);
+const auto2 = new Auto("Peugeot", "208", 5, 250000.0);
+const moto2 = new Moto("Yamaha", "YBR", "160c", 80500.5);
 
 listaDeVehiculos.agregar(auto1);
 listaDeVehiculos.agregar(moto1);
